dbda6c53d0144b1d8ecbfb3d128a389a8f724de7
josie <josie.mshrm@gmail.com>
2025-02-18 18:29:42 -0800

    added mouse support for camera control

--- souls/player/player_camera.gd
+++ souls/player/player_camera.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 var camera_input : Vector3
 var camera_rotation_sensitivity := 0.05
+var mouse_rotation_sensitvity := 0.05
 var rotation_limit_vertical := deg_to_rad(75)
 
 @onready var spring_arm: SpringArm3D = $SpringArm
@@ -14,7 +15,13 @@ func _physics_process(_delta: float) -> void:
 	
 	# vertical camera rotation
 	rotation.x += camera_input.z * camera_rotation_sensitivity
+	if InputController.mouse_camera:
+		rotation.x -= InputController.mouse_camera_movement.y * camera_rotation_sensitivity * mouse_rotation_sensitvity
+	
 	rotation.x = clampf(rotation.x, -rotation_limit_vertical, rotation_limit_vertical * 0.25)
 	
 	# horizontal camera rotation
 	rotation.y += camera_input.x * camera_rotation_sensitivity
+	if InputController.mouse_camera:
+		rotation.y -= InputController.mouse_camera_movement.x * camera_rotation_sensitivity * mouse_rotation_sensitvity
+		InputController.mouse_camera_movement = Vector2.ZERO
--- util/input_controller.gd
+++ util/input_controller.gd
@@ -2,8 +2,17 @@ extends Node
 
 signal player_input(action: Global.Action, event: InputEvent)
 
+@export var mouse_camera := true
+
 var player_movement := Vector3.ZERO
 var camera_movement := Vector3.ZERO
+var mouse_camera_movement := Vector2.ZERO
+
+
+func _ready() -> void:
+	if mouse_camera:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 
 func _physics_process(_delta: float) -> void:
 	player_movement.x = Input.get_axis(&"left", &"right")
@@ -12,9 +21,14 @@ func _physics_process(_delta: float) -> void:
 	camera_movement.x = Input.get_axis("cam - right", "cam - left")
 	camera_movement.z = Input.get_axis("cam - down", "cam - up")
 
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		player_input.emit(Global.Action.JUMP, event)
 	
 	if event.is_action_pressed("dash"):
 		player_input.emit(Global.Action.DASH, event)
+	
+	if mouse_camera:
+		if event is InputEventMouseMotion:
+			mouse_camera_movement = event.relative

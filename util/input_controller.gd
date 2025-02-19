ead60cc7b9127c8eeaf20076ede766777d41e1fa
josie.mshrm <josie.mshrm@gmail.com>
2025-02-18 17:25:10 -0800

    added controller to input map

--- util/input_controller.gd
+++ util/input_controller.gd
@@ -9,12 +9,12 @@ func _physics_process(_delta: float) -> void:
 	player_movement.x = Input.get_axis(&"left", &"right")
 	player_movement.z = Input.get_axis(&"up", &"down")
 	
-	camera_movement.x = Input.get_axis("cam - left", "cam - right")
-	camera_movement.z = Input.get_axis("cam - up", "cam - down")
+	camera_movement.x = Input.get_axis("cam - right", "cam - left")
+	camera_movement.z = Input.get_axis("cam - down", "cam - up")
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action("jump"):
+	if event.is_action_pressed("jump"):
 		player_input.emit(Global.Action.JUMP, event)
 	
-	if event.is_action("dash"):
+	if event.is_action_pressed("dash"):
 		player_input.emit(Global.Action.DASH, event)

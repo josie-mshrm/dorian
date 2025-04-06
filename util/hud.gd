d226d3bb2439863685486afb81d11867039e2147
josie <josie.mshrm@gmail.com>
2025-04-06 14:59:29 -0700

    save

--- util/hud.gd
+++ util/hud.gd
@@ -3,5 +3,5 @@ extends Control
 @export var parent : Soul
 @onready var progress_bar: ProgressBar = $ProgressBar
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	progress_bar.value = parent.health
